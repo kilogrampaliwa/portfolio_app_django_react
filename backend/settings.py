2b16ca05a64e344f1d6a3eddeafcb293b1ea91f0
Filip Ciąder <filip.ciader@gmail.com>
2025-04-22 21:42:32 +0200

    Changes in settings for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -91,7 +91,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #    }
 #}
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
